df5c9551c27c063af06557dd38809227804307e4
steamypotato <hoopahydra@gmail.com>
2020-02-09 13:31:56 -0500

    basic image upload from gallery functionnality

--- app/src/main/java/com/example/soen341/MainActivity.kt
+++ app/src/main/java/com/example/soen341/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.soen341
-
 import android.app.Activity
 import android.content.Intent
+import android.widget.Button;
 import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
@@ -21,56 +16,56 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        pickImageButton.setOnClickListener {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
-                    requestPermissions(permissions, PERMISSION_CODE);
-                }
-                else{
-                pickImageFromGallery();
-                }
+
+
+        upload_image_button.setOnClickListener {
+
+            if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            {
+                println("Permission denied");
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),MY_PERMISSIONS_READ_EXTERNAL_STORAGE)
+
             }
             else{
-
+                println("Permission already granted")
+                pickImageFromGallery()
             }
         }
     }
     private fun pickImageFromGallery(){
+        //this action for this intent will be a pick
         val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE);
-    }
-    companion object{
-        private val IMAGE_PICK_CODE = 1000;
-        private val PERMISSION_CODE = 1001;
+         intent.type = "image/*"
 
+        //handler function is overriden to handle the result of this request
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //check that we are responding to the image pick request and the request was sucesful
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            image_upload.setImageURI(data?.data)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         when(requestCode){
-            PERMISSION_CODE -> {
-                if(grantResults.size > 0 && grantResults[0]
-                    == PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery()
-                }
-                else{
-                    Toast.makeText(this,"Permission Denied Loser",Toast.LENGTH_SHORT).show();
+            MY_PERMISSIONS_READ_EXTERNAL_STORAGE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                } else{
+                    Toast.makeText(this,"Permisson denied loser",Toast.LENGTH_SHORT).show();
                 }
             }
         }
-       // super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            imageToUpload.setImageURI(data?.data);
-        }
+    companion object{
+        private val MY_PERMISSIONS_READ_EXTERNAL_STORAGE = 0;
+        private val IMAGE_PICK_CODE = 0;
     }
 }
