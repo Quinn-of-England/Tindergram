ddd16feedf5578b8cb94e0b45884041efa5155e1
Mark Zalass <markzalass@gmail.com>
2020-02-26 23:13:23 -0500

    Removed imports in RegisterActivity that were not being used - issue #29

--- app/src/main/java/com/example/soen341/RegisterActivity.kt
+++ app/src/main/java/com/example/soen341/RegisterActivity.kt
@@ -8,10 +8,8 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.AuthFailureError
-import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import org.json.JSONException
 import org.json.JSONObject
 
