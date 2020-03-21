1f7fed48922fb688283ce99d6e38623811ab0530
William <w.morin.laberge@gmail.com>
2020-03-21 19:11:37 -0400

    #69 Can return up to 5 pictures from a followed user

--- server/getBatchImages.php
+++ server/getBatchImages.php
@@ -8,18 +8,39 @@ $images = array();
                     $db = new DbOps();
                     $loggedId = $_GET['id'];
                     //get image rows of all pictures that were posted by users that the logged user follow
-                    $stmt = $db->conn->prepare("SELECT * FROM pictures WHERE authorId IN (SELECT id FROM users WHERE isFollowedBy LIKE '%?,%')");
-                    $stmt->bind_param("s", $loggedId);
+                    $regex = "'%".$loggedId.",%'";
+                    $preparedStatement = "SELECT * FROM pictures WHERE authorId IN (SELECT id FROM users WHERE isFollowedBy LIKE $regex)";
+                    //echo $preparedStatement;
+                    $stmt = $db->conn->prepare($preparedStatement);
                     if($stmt->execute()){
-			                  $stmt->store_result();
-			                  $stmt->bind_result($imageRowsFromFollowed);
-			                  $stmt->fetch();
+			                  // $stmt->store_result();
+			                  // $stmt->bind_result($imageRowsFromFollowed);
+			                  // $stmt->fetch();
+
+                        //will send a maximum of 5 pictures at once
+                        $imagesLeft = 5;
+                        $imageIndex = -1;
+                        $imageRowsFromFollowed = $stmt->get_result();
+                        while ($row = $imageRowsFromFollowed->fetch_assoc() and $imagesLeft >= 1) {
+                          //echo 'ID: '.$row['id'].'<br>';
+                          //array_push($images, $row['image']);
+                          $imageIndex = 5 - $imagesLeft;
+                          $response[$imageIndex] = base64_encode($row['image']);
+                          $imagesLeft--;
+                        }
+
+                        $response['error'] = 0;
+                        $response['size'] = $imageIndex + 1;
+                        $response['images'] = $images;
 
                         //for now, just select the first five images of the mysqli_more_results
                         //TODO ckeck the image ids to know whether they were viewed or not
 
-
-                        header("Content-Type: image/jpeg");
+                        //free results
+                        $stmt->free_result();
+                        //close statement
+                        $stmt->close();
+                        //header("Content-Type: image/jpeg");
 
 			              } else {
                       $response['error'] = 1;
