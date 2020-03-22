38233561fba0ee4c2c3df2a2328f95f78554e5d3
William <w.morin.laberge@gmail.com>
2020-03-22 02:06:31 -0400

    Notification column is cleared when it is checked

--- server/getNotification.php
+++ server/getNotification.php
@@ -5,15 +5,15 @@ $response = array();
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     if(isset($_GET['userId'])) {
       $db = new DbOps();
-      $notification = $db->getNotification($_GET['userId']);
-      if(substr($notification,0,1) !== ","){
+      $notification = $db->getAndClearNotification($_GET['userId']);
+      if(strlen($notification) != 0){
             // the image is now on the server, need to notify people that follow the author
             // hence it's better in the database if the follows section contains the people that follow that person.
             $response['error'] = false;
             $response['message'] = $notification." uploaded new pictures!";
         }else{
-            $response['error'] = true;
-            $response['message'] = "Error getting notification user";
+            $response['error'] = false;
+            $response['message'] = "None of the user(s) you follow has uploaded new content.";
         }
 
 		} else {
