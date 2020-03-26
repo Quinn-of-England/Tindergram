1bde33f34e37c38157a53a785548ea56688ce531
William <w.morin.laberge@gmail.com>
2020-03-26 15:21:47 -0400

    #81 backend will now send back pictures what weren't seen by the user

--- server/getBatchImages.php
+++ server/getBatchImages.php
@@ -5,6 +5,7 @@ $images = array();
 // takes id of the user
   if($_SERVER['REQUEST_METHOD'] == 'GET') {
                 if(isset($_GET['id'])) {
+                    //isset($_GET['imageIdsAlreadySeen']) not mandatory, will simply get same pictures if you don't include it
                     $db = new DbOps();
                     $loggedId = $_GET['id'];
                     //get image rows of all pictures that were posted by users that the logged user follow
@@ -23,15 +24,20 @@ $images = array();
                         $imageIndex = -1;
                         $imageRowsFromFollowed = $stmt->get_result();
                         while ($row = $imageRowsFromFollowed->fetch_assoc() and $imagesLeft >= 1) {
+                          $pattern1 = "/^".$row['id'].",/";
+                          $pattern2 = "/,".$row['id'].",/";
                           //echo 'ID: '.$row['id'].'<br>';
                           //array_push($images, $row['image']);
-                          $imageIndex = 5 - $imagesLeft;
-                          $response[$imageIndex]['image'] = base64_encode($row['image']);
-                          $response[$imageIndex]['id'] = $row['id'];
-                          $response[$imageIndex]['authorId'] = $row['authorId'];
-                          $response[$imageIndex]['likes'] = $row['likes'];
-                          $response[$imageIndex]['comments'] = $db->parseComments($row['comments']);
-                          $imagesLeft--;
+                          if(preg_match($pattern1, $_GET['imageIdsAlreadySeen']) == 0 and preg_match($pattern2, $_GET['imageIdsAlreadySeen']) == 0){
+                            $imageIndex = 5 - $imagesLeft;
+                            $response[$imageIndex]['image'] = base64_encode($row['image']);
+                            $response[$imageIndex]['id'] = $row['id'];
+                            $response[$imageIndex]['authorId'] = $row['authorId'];
+                            $response[$imageIndex]['likes'] = $row['likes'];
+                            $response[$imageIndex]['comments'] = $db->parseComments($row['comments']);
+                            $imagesLeft--;
+                          }
+
                         }
 
                         $response['error'] = 0;
