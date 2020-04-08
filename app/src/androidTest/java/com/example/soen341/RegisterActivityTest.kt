89afcccdd5e0a564d2ab3815b3f73d7fcb211034
steamypotato <hoopahydra@gmail.com>
2020-04-07 22:44:49 -0400

    register test actually create the account

--- app/src/androidTest/java/com/example/soen341/RegisterActivityTest.kt
+++ app/src/androidTest/java/com/example/soen341/RegisterActivityTest.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.espresso.Espresso.*
+import org.junit.Assert
 
 
 import org.junit.Test
@@ -33,13 +34,23 @@ import org.junit.Rule
 
     @Test
     fun user_can_register() {
-        onView(withId(R.id.username)).perform(typeText("test123"))
+        val applicationContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        RequestHandler.getInstance(applicationContext).registerUser(applicationContext,"test_account",
+            "test@email.com","test123",object : VolleyCallback{
+                override fun onResponse(response: MutableMap<String, String>?) {
+                    //check if the error flag from the network response is equal to 0, else assert using the message from the response
+                    assertTrue(response!!["message"],response!!["error"].equals("0"))
+                }
+            })
+        /*onView(withId(R.id.username)).perform(typeText("test123"))
         onView(withId(R.id.email)).perform(typeText("valid@email.com"))
         onView(withId(R.id.enterPass)).perform(typeText("test123"))
         onView(withId(R.id.confirmPass)).perform(typeText("test123"))
         closeSoftKeyboard()
         onView(withId(R.id.register)).perform(click())
-        val applicationContext = InstrumentationRegistry.getInstrumentation().targetContext
+        */
+
         assertFalse(SharedPrefManager.getInstance(applicationContext).isUserLoggedIn())
     }
 }
