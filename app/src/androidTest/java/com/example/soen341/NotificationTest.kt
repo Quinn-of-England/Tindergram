ccdf90a0924b3a4a72caf9d7a34a6e6a028b3264
Quinn <44041316+QuinnHogg01@users.noreply.github.com>
2020-04-08 15:06:29 -0400

    Added PostCommentTest

---
+++ app/src/androidTest/java/com/example/soen341/NotificationTest.kt
@@ -0,0 +1,13 @@
+package com.example.soen341
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NotificationTest
+{
+
+}
